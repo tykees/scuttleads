0c139ef2b9727af3c6f82912146bd2e9406a02b8
tykees <balogun.olu.sam@gmail.com>
2024-10-02 23:17:58 +0100

    auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from "next/font/google";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar/Navbar";
+// import Footer from "./components/Footer";
+// import Navbar from "./components/Navbar/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,10 +13,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider>
     <html lang="en">
-      <Navbar/>
       <body className={inter.className}>{children}</body>
-      <Footer/>
     </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,15 @@
-import Image from "next/image";
-import Cards from "./components/Cards";
-import Hero from "./components/Hero/Hero";
-import Services from "./components/Services";
-import Started from "./components/Started";
+import { UserButton } from "@clerk/nextjs";
+// import Cards from "./components/Cards";
+// import Hero from "./components/Hero/Hero";
+// import Services from "./components/Services";
+// import Started from "./components/Started";
 
 
 export default function Home() {
   
   return (
     <div>
-      <Hero/>
-      <Services/>
-      <Cards/>
-      <Started/>
+     <UserButton afterSignOutUrl="/sign-in" />
     </div>
   );
 }
